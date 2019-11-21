bb29548e0773e9b4726a1b81f5fb76ed31b81630
Hidehiko Naito <hidehiko@7110.email>
2019-11-22 04:53:45 +0900

    add CanvasCore.vue

---
+++ components/CanvasCore.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'CanvasCore',
+  props: {
+    item: {
+      required: true,
+      type: Object
+    },
+    // @TODO fix type
+    canvas: {
+      required: true,
+      type: undefined
+    }
+  },
+  render() {
+    const item = this.item
+    if (!(item.image && item.dw && item.dh && this.canvas)) return
+    const ctx = this.canvas.getContext('2d')
+    const [image, sx, sy, sw, sh, dx, dy, dw, dh] = [
+      item.image,
+      item.sx,
+      item.sy,
+      item.sw,
+      item.sh,
+      item.dx,
+      item.dy,
+      item.dw,
+      item.dh
+    ]
+    return ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+  }
+}
+</script>
