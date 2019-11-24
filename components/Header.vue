ececa6e0de06c0bf70834d8a5a03069dc7272222
Hidehiko Naito <hidehiko@7110.email>
2019-11-25 02:04:40 +0900

    add components/Header.vue

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="Header">
+    <h1 class="Header_main">
+      Square Image
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+.Header {
+  min-height: 48px;
+  padding: $si-basic-space * 2;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  border-bottom: 2px solid $si-color-border;
+
+  &_main {
+    font-size: 24px;
+  }
+
+  @include mediaQuery($query: 'PC') {
+    &_main {
+      width: 950px;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
