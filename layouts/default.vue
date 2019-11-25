fd7254365af291f0d508534b20d9c44568e13215
Hidehiko Naito <hidehiko@7110.email>
2019-11-26 06:32:57 +0900

    update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,36 @@
 <template>
-  <main>
-    <nuxt />
-  </main>
+  <div class="wrap">
+    <si-header />
+    <main>
+      <nuxt />
+    </main>
+  </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  box-sizing: border-box;
-}
+<script>
+import Header from '~/components/Header'
 
-*,
-*::before,
-*::after {
-  margin: 0;
-  box-sizing: border-box;
+export default {
+  components: {
+    'si-header': Header
+  }
+}
+</script>
+
+<style lang="scss">
+.wrap {
+  position: relative;
+
+  > .Header {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    background-color: $si-color-white;
+  }
+
+  > main {
+    display: block;
+    background-color: $si-color-light-gray;
+  }
 }
 </style>
