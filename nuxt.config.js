0f6504e79d510bbf8b4eac66c8743e6d7711dcb4
Hidehiko Naito <hidehiko@7110.email>
2019-11-26 06:43:57 +0900

    fix nuxt.config.js's miss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
-    ? { router: { base: '/<square-image>/' } }
+    ? { router: { base: '/square-image/' } }
     : {}
 
 export default {
