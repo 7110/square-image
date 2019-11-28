2cde93b92115e87020bc77ddf62f0f2cdda4899a
Hidehiko Naito <hidehiko@7110.email>
2019-11-28 23:28:09 +0900

    update Header.vue to add Logo image

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,11 @@
 <template>
   <header class="Header">
-    <h1 class="Header_main">
+    <h1 class="Header_title">
+      <img
+        src="/images/logo.png"
+        alt="Square Image ロゴ画像"
+        class="Header_logo"
+      />
       Square Image
     </h1>
   </header>
@@ -15,19 +20,29 @@ export default {
 <style lang="scss">
 .Header {
   min-height: $si-header-height;
-  padding: $si-basic-space * 2;
+  padding: $si-basic-space $si-basic-space * 4;
   overflow: hidden;
   display: flex;
   align-items: center;
   border-bottom: 2px solid $si-color-border;
 
-  &_main {
+  &_title {
     color: $si-color-text;
     font-size: 24px;
+    display: flex;
+    align-items: center;
+  }
+
+  &_logo {
+    width: 36px;
+    height: 36px;
+    margin-right: $si-basic-space * 4;
+    display: block;
+    flex-shrink: 0;
   }
 
   @include mediaQuery($query: 'PC') {
-    &_main {
+    &_title {
       width: 950px;
       margin: 0 auto;
     }
