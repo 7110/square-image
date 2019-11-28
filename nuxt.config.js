a84d0e0159245d16865946cad612788ec376ee59
Hidehiko Naito <hidehiko@7110.email>
2019-11-29 00:15:20 +0900

    update nuxt.config.js to add OGP, title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,9 +26,36 @@ export default {
       {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1'
-      }
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Square Image | 画像をセキュアに正方形へ'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://7110.github.io/square-image/'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Square Image | 画像をセキュアに正方形へ'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '通信せずにブラウザ上で画像を加工するからセキュリティ面でも安心・安全！'
+      },
+      { hid: 'og:image', property: 'og:image', content: '/images/ogp.png' },
+      { name: 'twitter:card', content: 'summary_large_image' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preload', as: 'image', href: '/images/logo.png' }
+    ]
   },
   /*
    ** Customize the progress-bar color
